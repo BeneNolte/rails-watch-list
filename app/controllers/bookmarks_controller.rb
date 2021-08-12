55548bed9eb58cd21fe5eff824d201cac454d009
Benedikt Nolte <bene_nolte@me.com>
2021-08-12 18:07:11 +0200

    Done

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,33 @@
+class BookmarksController < ApplicationController
+  def index
+
+  end
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+  end
+  def show
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
+  end
+
+  private
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
---
+++ app/models/bookmark.rb
@@ -0,0 +1,12 @@
+class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+
+  validates :comment, presence: true, length: { minimum: 6 }
+
+  validates :movie, presence: true
+  # validates :movie, format: { without: /\s/ }
+  validates :list, presence: true
+  validates_uniqueness_of :movie, :scope => [:list]
+
+end
